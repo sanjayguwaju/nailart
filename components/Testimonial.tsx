1488f59e64283e44794b174f6b419e393f03c3eb
smriteemanandhar <smriteemanandhar@gmail.com>
2023-12-08 22:46:28 +0545

    fix: quote issue 2 in testimonial

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -19,13 +19,13 @@ const Testimonial = () => {
                   fill="currentColor"
                 />
               </svg>
-
+              <blockquote>
                 <p className="text-2xl font-medium text-gray-900 dark:text-white">
-                  "Flowbite is just awesome. It contains tons of predesigned
+                  Flowbite is just awesome. It contains tons of predesigned
                   components and pages starting from login screen to complex
-                  dashboard. Perfect choice for your next SaaS application."
+                  dashboard. Perfect choice for your next SaaS application.
                 </p>
-
+                </blockquote>
               <figcaption className="flex items-center justify-center mt-6 space-x-3">
                 <Image
                   className="w-6 h-6 rounded-full"
