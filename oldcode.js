98356a681c4e90a3c244cb3871b02a950034760e
smriteemanandhar <smriteemanandhar@gmail.com>
2023-12-08 22:26:25 +0545

    feat: added testimonial in nailart

--- oldcode.js
+++ oldcode.js
@@ -12,15 +12,25 @@ import Nav from "@/components/Nav";
 import Foot from "@/components/Foot";
 import Slider from "@/components/Slider";
 import Services from "@/components/Services";
-import Testimonial from "@/components/Testimonial";
 
 export default function Home() {
   return (
     <>
+      {/* <Header /> */}
       <Nav/>
       <Slider/>
+      {/* <CarouselCode
+        images={[
+          "https://res.cloudinary.com/dz3facqgc/image/upload/v1700838318/tok8ygzylz5mp75yg3vq.webp",
+          "/images/bg-slider-3.jpg",
+          "/images/bg-slider-4.jpg",
+        ]}
+        titles={["GET A NEW LOOK FOR YOUR NAILS", "GET YOUR NAILS DONE BY A SKILLED NAIL ARTIST", "CHOOSE THE BEST ATTITUDE AND SERVICES"]}
+        subtitles={["Change Your Nails Look With Our Talented Stylists.", "Change Your Nails Look With Our Talented Stylists.", "Change Your Nails Look With Our Talented Stylists."]}
+      /> */}
+      {/* <OurServices /> */}
       <Services/>
-      <Testimonial/>
+      {/* <Footer /> */}
       <Foot/>
     </>
   );
